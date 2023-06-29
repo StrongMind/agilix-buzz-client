63ee9e9ba1c7e93556eabd929abc81f4dd8cb1fb
Megan Perry <megan.perry@strongmind.com>
2023-06-29 13:58:58 -0700

    (S) QTY-3448b: Adjust agilix rb
    Co-Authored-By: Angel Diaz <angel.diaz@strongmind.com>

--- lib/agilix.rb
+++ lib/agilix.rb
@@ -1,35 +1,15 @@
-
 # require "active_support/all"
 require "httparty"
 require "ostruct"
-
-require "agilix/buzz/commands/authentication"
-require "agilix/buzz/commands/course"
-require "agilix/buzz/commands/domain"
-require "agilix/buzz/commands/enrollment"
-require "agilix/buzz/commands/general"
-require "agilix/buzz/commands/library"
-require "agilix/buzz/commands/report"
-require "agilix/buzz/commands/resource"
-require "agilix/buzz/commands/right"
-require "agilix/buzz/commands/user"
-
-require "agilix/buzz/api"
 require "agilix/buzz/configuration"
-require "agilix/version"
-
-
-
 module Agilix
   class << self
     attr_accessor :configuration
   end
 
-
   autoload :Agilix, "agilix/version"
 
   autoload :Api, "agilix/buzz/api"
-  autoload :Configuration, "agilix/buzz/configuration"
 
   autoload :Authentication, "agilix/buzz/commands/authentication"
   autoload :Course, "agilix/buzz/commands/course"
@@ -42,9 +22,6 @@ module Agilix
   autoload :Right, "agilix/buzz/commands/right"
   autoload :User, "agilix/buzz/commands/user"
 
-  # Classes used to return a nicer object wrapping the response data
-  # autoload :Group, "schoology/objects/group"
-
   def self.configuration
     @configuration ||= Agilix::Buzz::Configuration.new
   end
@@ -54,9 +31,9 @@ module Agilix
   end
 
   def initialize
-    @agilix_buzz_username = self.class.configuration.oauth_consumer_key
-    @agilix_buzz_password = self.class.configuration.oauth_consumer_secret
-    @agilix_buzz_domain = self.class.configuration.url
+    @agilix_buzz_username = self.class.configuration.agilix_buzz_username
+    @agilix_buzz_password = self.class.configuration.agilix_buzz_password
+    @agilix_buzz_domain = self.class.configuration.agilix_buzz_domain
   end
 end
 
--- lib/agilix/buzz/configuration.rb
+++ lib/agilix/buzz/configuration.rb
@@ -1,12 +1,14 @@
-module Agilix::Buzz
-  class Configuration
-    attr_accessor :agilix_buzz_username, :agilix_buzz_password, :agilix_buzz_domain
+module Agilix
+  module Buzz
 
-    def initialize
-      @agilix_buzz_username = nil
-      @agilix_buzz_password = nil
-      @agilix_buzz_domain = nil
+    class Configuration
+      attr_accessor :agilix_buzz_username, :agilix_buzz_password, :agilix_buzz_domain
+
+      def initialize
+        @agilix_buzz_username = nil
+        @agilix_buzz_password = nil
+        @agilix_buzz_domain = nil
+      end
     end
-
   end
 end
