0c1e0fc461ee5293085ff01642e1c1bb4cd7d5d4
Angel Diaz <angel.diaz@strongmind.com>
2023-07-18 11:38:14 -0700

    (B) QTY-3640: add initial file for manifest commands

---
+++ lib/agilix/buzz/commands/manifest.rb
@@ -0,0 +1,11 @@
+module Agilix
+  module Buzz
+    module Commands
+      module Manifest
+        def update_manifest_data
+
+        end
+      end
+    end
+  end
+end
--- test/agilix/buzz/commands/manifest_test.rb
+++ test/agilix/buzz/commands/manifest_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-class Agilix::Buzz::Comands::ManifestTest < Minitest::Test
+class Agilix::Buzz::Commands::ManifestTest < Minitest::Test
   describe "#update_manifest_data" do
     it 'updates manifest data' do
-      VCR.use_cassette(Commands::Manifest update_manifest_data, match_requests_on: [:query]) do
+      VCR.use_cassette("Commands::Manifest update_manifest_data", match_requests_on: [:query]) do
         response = api.update_manifest_data [ { entityid: TEST_COURSE_ID }]
         assert_equal "OK", response.dig("response", "code")
       end
