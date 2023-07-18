bd6f6331bbd1efd29e0d8736480c590d382082e6
Angel Diaz <angel.diaz@strongmind.com>
2023-07-18 11:32:59 -0700

    (S) QTY-3640: add initial test for update_manifest_date

---
+++ test/agilix/buzz/commands/manifest_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class Agilix::Buzz::Comands::ManifestTest < Minitest::Test
+  describe "#update_manifest_data" do
+    it 'updates manifest data' do
+      VCR.use_cassette(Commands::Manifest update_manifest_data, match_requests_on: [:query]) do
+        response = api.update_manifest_data [ { entityid: TEST_COURSE_ID }]
+        assert_equal "OK", response.dig("response", "code")
+      end
+    end
+  end
+end
