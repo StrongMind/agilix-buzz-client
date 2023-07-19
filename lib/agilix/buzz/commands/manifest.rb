e915ad856bb8b370a01bfd390b2818b6a9423be6
Angel Diaz <angel.diaz@strongmind.com>
2023-07-19 09:40:07 -0700

    added new cassett for manifest_test

--- lib/agilix/buzz/commands/manifest.rb
+++ lib/agilix/buzz/commands/manifest.rb
@@ -2,8 +2,8 @@ module Agilix
   module Buzz
     module Commands
       module Manifest
-        def update_manifest_data
-
+        def update_manifest_data(manifest_data)
+          authenticated_post cmd: 'updatemanifestdata', manifestdata: manifest_data
         end
       end
     end
--- test/agilix/buzz/commands/manifest_test.rb
+++ test/agilix/buzz/commands/manifest_test.rb
@@ -4,8 +4,78 @@ class Agilix::Buzz::Commands::ManifestTest < Minitest::Test
   describe "#update_manifest_data" do
     it 'updates manifest data' do
       VCR.use_cassette("Commands::Manifest update_manifest_data", match_requests_on: [:query]) do
-        response = api.update_manifest_data [ { entityid: TEST_COURSE_ID }]
-        assert_equal "OK", response.dig("response", "code")
+        response = api.update_manifest_data({ "Manifests": [
+          {
+            "EntityId": "196363507",
+            "Data": {
+              "AllowStaticCopy": false,
+              "AllowDerivativeChildCopy": true,
+              "AllowDerivativeSiblingCopy": false,
+              "GradeCategories": {
+                "Weighted": true,
+                "Categories": [
+                  {
+                    "Id": 1,
+                    "GradeView": 0,
+                    "Name": "Checkpoint",
+                    "AlphaIndex": "aa",
+                    "Weight": 15
+                  },
+                  {
+                    "Id": 2,
+                    "GradeView": 0,
+                    "Name": "Discussion",
+                    "AlphaIndex": "ab",
+                    "Weight": 15
+                  },
+                  {
+                    "Id": 3,
+                    "GradeView": 0,
+                    "Name": "Exam",
+                    "AlphaIndex": "ac",
+                    "Weight": 15
+                  },
+                  {
+                    "Id": 4,
+                    "GradeView": 0,
+                    "Name": "Final Exam",
+                    "AlphaIndex": "ad",
+                    "Weight": 15
+                  },
+                  {
+                    "Id": 5,
+                    "GradeView": 0,
+                    "Name": "Pretest",
+                    "AlphaIndex": "ae",
+                    "Weight": 0
+                  },
+                  {
+                    "Id": 6,
+                    "GradeView": 0,
+                    "Name": "Project",
+                    "AlphaIndex": "af",
+                    "Weight": 30
+                  },
+                  {
+                    "Id": 7,
+                    "GradeView": 0,
+                    "Name": "Workbook",
+                    "AlphaIndex": "ag",
+                    "Weight": 10
+                  }
+                ]
+              },
+              "Description": "",
+              "GradeView": "",
+              "ThumbnailPath": "",
+              "LandingPagePath": {
+                "Value": "assets/syllabus.html"
+              }
+            }
+          }
+        ]
+                                            })
+        puts "manifest response: #{response.inspect}"
       end
     end
   end
